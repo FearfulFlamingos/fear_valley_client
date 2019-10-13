4cafc777fce6387e5a568cbb3cab84e681bb24c0
Gavin Lochtefeld <lochga01@luther.edu>
2019-10-13 17:31:21 -0500

    Started UI
    Added UI Dropdown menus + dynamic budget text. Need grid + add person on click.

---
+++ fear_hello_world/Assets/Scripts/ArmyBudget.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArmyBudget : MonoBehaviour
+{
+    private int currentBudget = 300;
+    private DBStandin newDB = new DBStandin();
+    // Start is called before the first frame update
+    public Text displayBudget;
+    public Dropdown characters;
+    public Dropdown weapons;
+    public Dropdown armors;
+
+
+    private void Start()
+    {
+        PopulateDropdown(characters, newDB.getArmy());
+        PopulateDropdown(weapons, newDB.getWeapons());
+        PopulateDropdown(armors, newDB.getArmor());
+    }
+
+    private void Update()
+    {
+        displayBudget.text = $"Current Resources: {currentBudget}";
+    }
+
+    private int calculateCost(string army, string weapon, string armor)
+    {
+        return 10; // Just here to avoid throwing an error for now
+    }
+
+    private void PopulateDropdown(Dropdown drpdwn, Dictionary<string, int> optionsArray)
+    {
+        List<string> options = new List<string>();
+        foreach (var option in optionsArray.Keys)
+        {
+            options.Add(option);
+        }
+        drpdwn.ClearOptions();
+        drpdwn.AddOptions(options);
+    }
+}
+
+class DBStandin
+{
+    Dictionary<string, int> armyType = new Dictionary<string, int>()
+        { {"None",0 }, {"Peasant",10}, {"Warrior",50},{"Mage", 100}  };
+    Dictionary<string, int> weaponType = new Dictionary<string, int>()
+        { {"None",0 }, {"1H",15 },{"2H", 20},{"Polearm",10},{"Ranged",25} };
+    Dictionary<string, int> armorType = new Dictionary<string, int>()
+        {{"None",0 },
+        { "Light Mundane",20},
+        { "Light Magic",40},
+        { "Heavy Mundane",30},
+        { "Heavy Magic",50},
+        { "Mundane Shield", 10},
+        { "Magic Shield",30 }};
+    
+    public Dictionary<string,int> getArmy()
+    {
+        return armyType;
+    }
+    public Dictionary<string, int> getWeapons()
+    {
+        return weaponType;
+    }
+    public Dictionary<string, int> getArmor()
+    {
+        return armorType;
+    }
+}
