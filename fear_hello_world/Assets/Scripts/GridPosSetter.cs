e48f5bc818835dcceaba5b4b6e54a25c08d7ec84
Gavin Lochtefeld <lochga01@luther.edu>
2019-10-27 15:51:19 -0500

    Added buttons for grid

---
+++ fear_hello_world/Assets/Scripts/GridPosSetter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPosSetter : MonoBehaviour
+{
+    [SerializeField]
+    private int pos_x;
+    [SerializeField]
+    private int pos_y;
+    
+    private Color selectedColor = new Color(0, 233, 128);
+    private Color normalColor = new Color(156, 144, 144);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int GetXPos()
+    {
+        return pos_x;
+    }
+    private int GetYPos()
+    {
+        return pos_y;
+    }
+
+
+}
