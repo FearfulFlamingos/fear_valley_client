e5037f67ae3bc9e70c5b04450c7c715e7cdc1011
Nathan Schulte <schuna05@nathans-air.lc.luther.edu>
2019-11-13 18:55:59 -0600

    downloaded game with menus

---
+++ fear_hello_world/Assets/Data/create_weapon_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS Weapon (
+id integer PRIMARY KEY,
+name text NOT NULL,
+cost float,
+damage float,
+attack float,
+range float
+);
---
+++ fear_hello_world/Assets/Scripts/CharacterFeatures.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFeatures : MonoBehaviour
+{
+    public int team;
+    public int health;
+    public string charclass;
+    public string armclass;
+    public string shield;
+    public string weapon;
+    public int isLeader;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
