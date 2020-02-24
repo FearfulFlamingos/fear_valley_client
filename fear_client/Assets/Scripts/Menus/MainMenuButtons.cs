7a04a741d5e466a659eaebd26f80f8e77cc85d00
Gavin Lochtefeld <lochga01@luther.edu>
2020-02-24 15:08:09 -0600

    removed Host button

--- fear_client/Assets/Scripts/Menus/MainMenuButtons.cs
+++ fear_client/Assets/Scripts/Menus/MainMenuButtons.cs
@@ -7,15 +7,14 @@ using TMPro;
 
 public class MainMenuButtons : MonoBehaviour
 {
-    public Button playButton, tutorialButton, hostButton, quitButton, creditsButton, optionsButton;
+    public Button playButton, tutorialButton, quitButton, creditsButton, optionsButton;
     public Canvas mainCanvas, optionsCanvas, creditsCanvas;
     public Button optionsReturn, creditsReturn;
 
     void Start()
     {
-        playButton.onClick.AddListener(() => SceneManager.LoadScene(1));
-        tutorialButton.onClick.AddListener(() => SceneManager.LoadScene(1));
-        hostButton.onClick.AddListener(() => StartHost());
+        playButton.onClick.AddListener(() => SceneManager.LoadScene("ServerConnect"));
+        tutorialButton.onClick.AddListener(() => SceneManager.LoadScene("TempLoadingScene"));
         quitButton.onClick.AddListener(() => Application.Quit());
         creditsButton.onClick.AddListener(() => SwitchCanvas(mainCanvas,creditsCanvas));
         optionsButton.onClick.AddListener(() => SwitchCanvas(mainCanvas,optionsCanvas));
@@ -30,10 +29,4 @@ public class MainMenuButtons : MonoBehaviour
         to.gameObject.SetActive(true);
     }
 
-    private void StartHost()
-    {
-        GameObject serverObject = (GameObject) Instantiate(Resources.Load("Server"));
-        DontDestroyOnLoad(serverObject);
-        SceneManager.LoadScene("ArmyBuild");
-    }
 }
