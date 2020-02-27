a3c9316fa365b41a93cc11f499b524ed85288722
Gavin Lochtefeld <lochga01@luther.edu>
2020-02-26 20:02:17 -0600

    No TMPro namespace errors

--- fear_client/Assets/Scripts/Character/GameLoop.cs
+++ fear_client/Assets/Scripts/Character/GameLoop.cs
@@ -28,6 +28,9 @@ public class GameLoop : MonoBehaviour
     }
 
     // Update is called once per frame
+    /// <summary>
+    /// This function is constantly checking if action points have dipped below 0 at which point the next turn is triggered
+    /// </summary>
     void Update()
     {
         if (actionPoints == 0)
@@ -39,7 +42,13 @@ public class GameLoop : MonoBehaviour
         }
 
     }
-
+    /// <summary>
+    /// This function is used to create dictionaries for each player. The dicitionaries are used to track
+    /// the number of active figures each player currently has and it tracks a reference to the gameobjects.
+    /// </summary>
+    /// <param name="team"></param>
+    /// <param name="troopid">Incremental Id goes up by 1 for each new troop</param>
+    /// <param name="reference">Reference to the GameObject</param>
     public void AddtoDict(int team,int troopid, GameObject reference)
     {
         if (team == 1)
@@ -51,6 +60,14 @@ public class GameLoop : MonoBehaviour
             p2CharsDict.Add(troopid, reference);
         }
     }
+    /// <summary>
+    /// This is a networking function that is called by the client script eachtime the other player moves.
+    /// Once this message is received the gameobject is retrieved from the dictionary and then the move
+    /// function is triggered.
+    /// </summary>
+    /// <param name="troopid"></param>
+    /// <param name="newX"></param>
+    /// <param name="newZ"></param>
     public void MoveOther(int troopid, float newX, float newZ)
     {
         GameObject changing = p2CharsDict[troopid];
@@ -62,23 +79,40 @@ public class GameLoop : MonoBehaviour
         Debug.Log(newVect);
         changing.GetComponent<PlayerMovement>().MoveMe(newVect);
     }
+    /// <summary>
+    /// This is another networking function and it currently is used to remove health from a specified
+    /// troop after another player succesfully attacks them.
+    /// </summary>
+    /// <param name="troopid"></param>
+    /// <param name="damage"></param>
     public void IveBeenAttacked(float troopid, float damage)
     {
         GameObject changing = p1CharsDict[System.Convert.ToInt32(troopid)];
         CharacterFeatures reference = changing.GetComponent<CharacterFeatures>();
         reference.health = System.Convert.ToInt32(reference.health - damage);
     }
+    /// <summary>
+    /// This is called after an attack is triggered and sends the message to the player attack script.
+    /// </summary>
     public void Attack()
     {
         lastClicked.GetComponent<PlayerAttack>().Attack();
         actionPoints--;
     }
+
+    /// <summary>
+    /// This function is called after the cancel attack button is pressed and it deactivates attack mode.
+    /// </summary>
     public void CancelAttack()
     {
         attackcanvas.SetActive(false);
         lastClicked.GetComponent<PlayerAttack>().DeactivateAttack();
         lastClicked.GetComponent<PlayerAttack>().enabled = false;
     }
+    /// <summary>
+    /// This function is used after the end of an attack to deactivate attack mode and track the number
+    /// of attacks.
+    /// </summary>
     public void EndAttack()
     {
         attackcanvas.SetActive(false);
@@ -87,10 +121,18 @@ public class GameLoop : MonoBehaviour
         numAttacks++;
         //actionPoints++;
     }
+    /// <summary>
+    /// This function is called when the game is finished and the player clicks the x button.
+    /// </summary>
     public void endGame()
     {
         SceneManager.LoadScene("ArmyBuilder");
     }
+    /// <summary>
+    /// This is a networking function used when other players call a retreat in the game.
+    /// </summary>
+    /// <param name="troopId">Troop retreating</param>
+    /// <param name="TeamNum">The team that is leaving</param>
     public void OtherLeaves(int troopId,int TeamNum)
     {
         Debug.Log($"Retreat message received with {TeamNum} and {troopId}");
@@ -108,6 +150,9 @@ public class GameLoop : MonoBehaviour
         }
             
     }
+    /// <summary>
+    /// This function is called when the local player calls a retreat on their figure.
+    /// </summary>
     public void Leave()
     {
         if (scripts.GetComponent<GameLoop>().actionPoints >= 3)
@@ -122,6 +167,14 @@ public class GameLoop : MonoBehaviour
 
         }
     }
+
+    /// <summary>
+    /// This function is checked each time any damage is done. It checks to see if the player's health is
+    /// 0. Following this the function checks if the endgame has been trigged.
+    /// </summary>
+    /// <param name="action"></param>
+    /// <param name="troopId"></param>
+    /// <param name="playerInQuestion"></param>
     public void PlayerRemoval(string action, int troopId, int playerInQuestion)
     {
         if (action == "Retreat")
@@ -133,7 +186,7 @@ public class GameLoop : MonoBehaviour
                 {
                     victorycanvas.SetActive(true);
                     attackcanvas.SetActive(false);
-                    victoryStatement.text = $"You have fled the battlefield. ";
+                    //victoryStatement.text = $"Victory has been acheived for \nplayer 2 after player 1 retreated ";
                 }
             }
             else
@@ -143,7 +196,7 @@ public class GameLoop : MonoBehaviour
                 {
                     victorycanvas.SetActive(true);
                     attackcanvas.SetActive(false);
-                    victoryStatement.text = $"Your enemy has retreated!";
+                    //victoryStatement.text = $"Victory has been acheived for \nplayer 1 after player 2 retreated ";
                 }
             }
         }
@@ -156,7 +209,7 @@ public class GameLoop : MonoBehaviour
                 {
                     victorycanvas.SetActive(true);
                     attackcanvas.SetActive(false);
-                    victoryStatement.text = $"Your foes lie defeated! Victory!";
+                    victoryStatement.text = $"Victory has been acheived for \nplayer 1 after defeating player 2 ";
                 }
             }
             else
@@ -166,7 +219,7 @@ public class GameLoop : MonoBehaviour
                 {
                     victorycanvas.SetActive(true);
                     attackcanvas.SetActive(false);
-                    victoryStatement.text = $"You have suffered a terrible defeat.";
+                    victoryStatement.text = $"Victory has been acheived for \nplayer 2 after defeating player 1 ";
                 }
             }
         }
