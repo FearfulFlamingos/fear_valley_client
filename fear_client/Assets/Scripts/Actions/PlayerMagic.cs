09fa17734bf3258c8266851d0d056ec67e5d2c25
Gavin Lochtefeld <lochga01@luther.edu>
2020-03-03 16:47:35 -0600

    Magic Attack completed*
    * Players can't currently start with any magic explosions

--- fear_client/Assets/Scripts/Actions/PlayerMagic.cs
+++ fear_client/Assets/Scripts/Actions/PlayerMagic.cs
@@ -9,7 +9,14 @@ public class PlayerMagic : MonoBehaviour
     private bool placingExplosion = false;
     private Vector3 startingPosition;
     private GameObject selection;
+    private GameObject scripts;
+
     public ParticleSystem explosionEffect;
+
+    private void Start()
+    {
+        scripts = GameObject.FindGameObjectWithTag("scripts");
+    }
 
     // Update is called once per frame
     private void Update()
@@ -20,9 +27,8 @@ public class PlayerMagic : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 100.0f, 1 << 9)) //Only look on ground layer
             {
-                if (Vector3.Distance(hit.point, startingPosition) < 2)
+                if (Vector3.Distance(hit.point, startingPosition) < 20)
                 {
-                    Debug.Log("Moving");
                     selection.transform.position = hit.point;
                 } 
                 
@@ -40,6 +46,13 @@ public class PlayerMagic : MonoBehaviour
         placingExplosion = false;
         ParticleSystem ex = Instantiate(explosionEffect, selection.transform.position, Quaternion.identity);
         ex.Play();
+
+        HashSet<GameObject> enemies = selection.GetComponent<BlowUpEnemies>().GetEnemiesInBlast();
+        foreach (GameObject enemy in enemies)
+        {
+            MagicAttack(enemy);
+        }
+
         Destroy(selection);
         
     }
@@ -51,5 +64,40 @@ public class PlayerMagic : MonoBehaviour
         selection.transform.position = startingPosition;
         placingExplosion = true;
     }
-        
+
+    public void MagicAttack(GameObject enemy)
+    {
+        System.Random random = new System.Random();
+        CharacterFeatures referenceScript = enemy.GetComponent<CharacterFeatures>();
+        CharacterFeatures referenceScript2 = gameObject.GetComponent<CharacterFeatures>();
+        GameLoop gamevars = scripts.GetComponent<GameLoop>();
+
+            if (random.Next(0, 20) + referenceScript2.magicattack >= referenceScript.bonus)
+            {
+                int damageTaken = random.Next(1, 12) + referenceScript2.magicdamage;
+                if ((referenceScript.health - damageTaken) <= 0)
+                {
+                    //attackChar.text = $"You have dealt fatal damage\nto the player named Roman ";
+                    Debug.Log("Killed enemy");
+
+                    gamevars.PlayerRemoval("Attack", enemy.GetComponent<CharacterFeatures>().troopId, 2);
+                    Client.Instance.SendRetreatData(referenceScript.troopId, 1);
+                }
+                else
+                {
+                    referenceScript.health = System.Convert.ToInt32(referenceScript.health - damageTaken);
+                    Client.Instance.SendAttackData(referenceScript.troopId, damageTaken);
+                //attackChar.text = $"You attack was a success \nand you have dealt {damageTaken} damage\nto the player named Roman ";
+                Debug.Log($"Dealt {damageTaken} damage");
+                }
+
+            }
+            else
+            {
+            //attackChar.text = $"You could not get passed their armor\nyour attack has failed";
+            Debug.Log("Missed attack");
+            }
+
+    }
+
 }
