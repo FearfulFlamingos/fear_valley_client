5dfdfd7f55808a22ba4fcac1bf65208bd3a4edb3
Gavin Lochtefeld <lochga01@luther.edu>
2020-04-29 22:45:44 -0500

    Unfudged everything
    So to speak.

--- fear_client/Assets/Scripts/Menus/MainMenuButtons.cs
+++ fear_client/Assets/Scripts/Menus/MainMenuButtons.cs
@@ -12,6 +12,10 @@ namespace Scripts.Menus
         public Button playButton, tutorialButton, quitButton, creditsButton, optionsButton;
         public Canvas mainCanvas, optionsCanvas, creditsCanvas;
         public Button optionsReturn, creditsReturn;
+        public TMP_InputField displayName;
+        public TMP_Text storedName, gamesPlayed, gamesWon;
+        public GameObject nameSetAlert;
+        public Button setDisplayName;
 
         public static Canvas MainCanvas { set; get; }
         public static Canvas OptionsCanvas { set; get; }
@@ -24,9 +28,25 @@ namespace Scripts.Menus
             optionsButton.onClick.AddListener(() => SwitchCanvas(mainCanvas, optionsCanvas));
             optionsReturn.onClick.AddListener(() => SwitchCanvas(optionsCanvas, mainCanvas));
             creditsReturn.onClick.AddListener(() => SwitchCanvas(creditsCanvas, mainCanvas));
+            setDisplayName.onClick.AddListener(() => 
+            {
+                if (displayName.text != "")
+                {
+                    PlayerPrefs.SetString("PlayerName", displayName.text);
+                    nameSetAlert.GetComponent<CanvasGroup>().alpha = 1;
+                    StartCoroutine(FadeOut(nameSetAlert.GetComponent<CanvasGroup>()));
+                    storedName.SetText($"Name: {displayName.text}");
+                    displayName.text = "";
+                }
+            });
 
             MainCanvas = mainCanvas;
             OptionsCanvas = optionsCanvas;
+            int gamesPlayedVal = PlayerPrefs.HasKey("GamesPlayed") ? PlayerPrefs.GetInt("GamesPlayed") : 0;
+            int gamesWonVal = PlayerPrefs.HasKey("GamesWon") ? PlayerPrefs.GetInt("GamesWon") : 0;
+            storedName.SetText("Name: " + PlayerPrefs.GetString("PlayerName"));
+            gamesPlayed.SetText($"Games Played: {gamesPlayedVal}");
+            gamesWon.SetText($"Games Won: {gamesWonVal}");
 
         }
 
@@ -36,5 +56,15 @@ namespace Scripts.Menus
             to.gameObject.SetActive(true);
         }
 
+        public IEnumerator FadeOut(CanvasGroup canvasGroup)
+        {
+            const float RATE = 0.01f;
+            yield return new WaitForSeconds(1);
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= RATE;
+                yield return null;
+            }
+        }
     }
 }
